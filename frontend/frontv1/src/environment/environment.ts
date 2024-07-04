b132125daa34e914f3f09a4633fa67e2ff99e5bb
fernandacodes <fernanda.ferreira@icomp.ufam.edu.br>
2024-07-04 14:29:29 -0400

    adding configuration for production

--- frontend/frontv1/src/environment/environment.ts
+++ frontend/frontv1/src/environment/environment.ts
@@ -1,6 +1,6 @@
 let environment = "production";
 const BASE_URL_DEV = 'http://localhost:8000';
-const BASE_URL_PRD = 'https://web:8000';
+const BASE_URL_PRD = 'http://18.230.148.244:8000';
 
 // Exportando as constantes para uso em outras partes do seu aplicativo
 export const API_BASE_URL = environment === "production"? BASE_URL_PRD : BASE_URL_DEV;
