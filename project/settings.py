9a952444aa6d025974424cd9c2c3874cadefae86
fernandacodes <fernanda.ferreira@icomp.ufam.edu.br>
2024-09-09 14:07:07 -0300

    fix:adding ip from ec2

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "web",
     "http://localhost:3000"
-    "15.228.45.143"  # IP público da instância EC2
+    "15.228.45.143:8000" 
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -39,7 +39,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1",
-    "http://15.228.45.143"  # IP público da instância EC2 com protocolo
+    "http://15.228.45.143:8000"
 ]
 
 SIMPLE_JWT = {
