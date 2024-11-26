98c555444e7a0c356351db06f77b5481fc380dbd
fernandaferreiraK <fernanda.ferreira@keltecnologia.com.br>
2024-11-26 15:05:50 -0300

    feat : adding new script to charge

--- charge/charge.py
+++ charge/charge.py
@@ -3,26 +3,27 @@ import requests
 import logging
 from tqdm import tqdm
 from tratar_dados import TratarCidadesBrasil
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def get_api_url():
-    base_url = 'http://localhost:8000/api/register/' 
-    environment = 'local' 
-    
+    environment = os.getenv('ENVIRONMENT', 'development')
     if environment == 'production':
-        base_url = 'http://15.228.45.143:8000/api/register/' 
-    
-    return base_url
+        return os.getenv('API_URL_PROD')
+    return os.getenv('API_URL_DEV')
 
 tratar_cidades = TratarCidadesBrasil()
 
-def register_students(data):
+def register_user(data):
     url = get_api_url()
     try:
         response = requests.post(url, json=data)
-        response.raise_for_status() 
+        response.raise_for_status()
         return response
     except requests.exceptions.RequestException as e:
         logger.error(f"Erro ao enviar requisição para {url}: {e}")
@@ -31,21 +32,16 @@ def register_students(data):
         return None
 
 def generate_password(row):
-    if row['Matricula']:
-        return row['Matricula']
-    else:
-        return row['CPF'] + row['Data'].replace('-', '')
+    first_name = row['Nome'].split()[0]
+    return row['CPF'] + first_name[:2].lower()
 
 def generate_enrollment_number(row):
-    if row['Matricula']:
-        return row['Matricula']
-    else:
-        return 'X'
+    return row['Matricula'] if row['Matricula'] else 'X'
 
-def send_requests_from_csv(csv_file):
+def send_requests_from_csv(csv_file, role):
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        total_rows = sum(1 for row in reader) 
+        total_rows = sum(1 for row in reader)
 
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -54,58 +50,68 @@ def send_requests_from_csv(csv_file):
         for row in reader:
             password = generate_password(row)
             enrollment_number = generate_enrollment_number(row)
-            
             municipio_padronizado = tratar_cidades.tratar_cidade(
-                tratar_cidades.listar_cidades(), 
+                tratar_cidades.listar_cidades(),
                 row['Município']
             )
-            
-            student_data = {
+
+            user_data = {
                 'user': {
-                    'username': row['CPF'],  
-                    'password': password, 
+                    'username': row['CPF'],
+                    'password': password,
                     'email': row['Email'],
                     'cpf': row['CPF'],
                     'first_name': row['Nome'].split()[0],
                     'last_name': " ".join(row['Nome'].split()[1:]),
-                    'role': 'student'
-                },
-                'student': {
-                    'CPF': row['CPF'],
-                    'Name': row['Nome'],
-                    'Email': row['Email'],
-                    'Phone': row['Telefone'],
-                    'Municipality': municipio_padronizado,
-                    'State': row['UF'],
-                    'Status': row['Situação'],
-                    'Cycle': row['Ciclo'],
-                    'List': row['Lista'],
-                    'DSEI': row['DSEI'].lower() in ['true', '1', 't', 'yes', 'y', 'sim', 's'],
-                    'EnrollmentNumber': enrollment_number,
-                    'TutorClass': row['TurmaTutor'],
-                    'Date': row['Data'],
-                    'Condition': row['Condição'],
-                    'RequestChangeDate': row.get('RequestChangeDate')
+                    'role': role
                 }
             }
 
-            logger.info(f"Enviando dados do aluno: {student_data}")
-            response = register_students(student_data)
+            if role == 'student':
+                student_data = {
+                    'student': {
+                        'CPF': row['CPF'],
+                        'Name': row['Nome'],
+                        'Email': row['Email'],
+                        'Phone': row['Telefone'],
+                        'Municipality': municipio_padronizado,
+                        'State': row['UF'],
+                        'Status': row['Situação'],
+                        'Cycle': row['Ciclo'],
+                        'List': row['Lista'],
+                        'DSEI': row['DSEI'].lower() in ['true', '1', 't', 'yes', 'y', 'sim', 's'],
+                        'EnrollmentNumber': enrollment_number,
+                        'TutorClass': row['TurmaTutor'],
+                        'Date': row['Data'],
+                        'Condition': row['Condição'],
+                        'RequestChangeDate': row.get('RequestChangeDate')
+                    }
+                }
+                user_data.update(student_data)
+
+            logger.info(f"Enviando dados do {role}: {user_data}")
+            response = register_user(user_data)
 
             if response and response.status_code == 201:
                 progress_bar.set_postfix({'status': 'success'})
             else:
                 progress_bar.set_postfix({'status': 'error'})
-
                 if response:
-                    logger.error(f"Erro ao registrar aluno {row['Nome']}: {response.status_code} - {response.content}")
+                    logger.error(f"Erro ao registrar {role} {row['Nome']}: {response.status_code} - {response.content}")
                 else:
-                    logger.error(f"Erro ao registrar aluno {row['Nome']}: Erro de conexão")
+                    logger.error(f"Erro ao registrar {role} {row['Nome']}: Erro de conexão")
 
             progress_bar.update(1)
 
         progress_bar.close()
 
 if __name__ == '__main__':
-    csv_file_path = './data/students.csv'
-    send_requests_from_csv(csv_file_path)
+    students_csv_path = './data/students.csv'
+    admin_csv_path = './data/admin.csv'
+
+    send_requests_from_csv(students_csv_path, role='student')
+
+    if os.path.getsize(admin_csv_path) > 0:
+        send_requests_from_csv(admin_csv_path, role='admin')
+    else:
+        logger.info("Arquivo admin.csv está vazio, ignorando o cadastro de administradores.")
